9e43ede10cfb1fbf79e98ab1358b9c07d0070ace
Leo Bjerner <leonard.bjerner@gmail.com>
2026-04-05 11:19:25 +0200

    added Interface and fetchAnimals

--- src/modules/fetchAnimals.ts
+++ src/modules/fetchAnimals.ts
@@ -1,5 +1,20 @@
 //Använd fetch för att hämta datan i data/data.json
 //Använd try/catch och om anropet lyckas, returnera datan. Annars, returnera "null"
 //Typa upp funktionen med vad den returnerar
+import { IAnimal } from "./IAnimal.ts";
 
-export default async function fetchAnimals() {}
+export default async function fetchAnimals(): Promise<IAnimal[] | null> {
+  try {
+    const response = await fetch("data/data.json");
+
+    if (response.ok) {
+      const result = await response.json();
+      return result;
+    } else {
+      throw new Error("Could not fetch animal");
+    }
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,6 @@
 import fetchAnimals from "./modules/fetchAnimals.ts";
 import renderListOfAnimals from "./modules/renderListOfAnimals.ts";
+import { IAnimal } from "./modules/IAnimal";
 
 async function init() {
   const animals = await fetchAnimals();
